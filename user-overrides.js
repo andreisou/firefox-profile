f1600654748ab621852ff50ca33f643f995305cc
Andrei de Souza <neraidwastaken@gmail.com>
2023-04-20 14:20:39 -0300

    add user.js use my own user.js based off of arkenfox's

--- user-overrides.js
+++
@@ -1,6 +0,0 @@
-user_pref("browser.uidensity", 1);
-user_pref("extensions.activeThemeID", "firefox-compact-dark@mozilla.org");
-user_pref("keyword.enabled", true);
-user_pref("signon.rememberSignons", false);
-user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
-user_pref("browser.eme.ui.enabled", false);
